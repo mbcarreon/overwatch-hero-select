866666ccf3640c2805a2b8a4e8de6ad523f5e59a
mb.carreon <margaretcarreon19@gmail.com>
2024-04-23 10:10:45 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,46 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import heroesData from './heroes.json';
 
 const heroes = ref(heroesData);
-
-const videoRef = ref(null);
-
 const currentHero = ref(0);
+const videoRef = ref(null);
+const currentAbility = ref(null);
 
 const changeHero = (id) => {
   currentHero.value = id - 1;
   const video = videoRef.value;
-  video.load();
+  if (video) video.load();
 };
+
+const changeAbility = (abilityName) => {
+  const selectedHero = heroes.value[currentHero.value];
+  const selectedAbility = selectedHero.abilities.find(ability => ability.name === abilityName);
+  currentAbility.value = { ...selectedAbility };
+};
+
+onMounted(() => {
+  if (heroes.value[currentHero.value].abilities.length > 0) {
+    changeAbility(heroes.value[currentHero.value].abilities[0].name);
+  }
+});
 </script>
 
 <template>
   <div class="main-container">
-    <video autoplay muted loop id="background-video" ref="videoRef">
+    <video autoplay loop id="background-video" ref="videoRef">
       <source :src="heroes[currentHero].background" type="video/mp4">
     </video>
     <div class="hero-details-container">
       <h2>{{ heroes[currentHero].role }}</h2>
       <h1>{{ heroes[currentHero].name }}</h1>
       <div class="abilities-container">
-        <div class="abilities-sub-container"></div>
-        <div class="abilities-sub-container"></div>
-        <div class="abilities-sub-container"></div>
-        <div class="abilities-sub-container"></div>
-        <div class="abilities-sub-container"></div>
+        <div class="abilities-sub-container" v-for="ability in heroes[currentHero].abilities" :key="ability.name" @click="changeAbility(ability.name)">
+          <img :src="ability.icon">
+        </div>
       </div>
-      <h3>Ability</h3>
-      <h4>Lorem ipsum</h4>
+      <h3 v-if="currentAbility">{{ currentAbility.name }}</h3>
+      <h4 v-if="currentAbility">{{ currentAbility.description }}</h4>
     </div>
     <div class="heroes-container">
       <div class="heroes-sub-container" v-for="hero in heroes" :key="hero.id" @click="changeHero(hero.id)">
@@ -45,13 +54,6 @@ const changeHero = (id) => {
 </template>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Figtree:wght@300&family=Instrument+Serif:ital@0;1&display=swap');
-
-* {
-  color: white;
-  font-family: "Bebas Neue";
-}
-
 .main-container {
   width: 100%;
   padding: 48px;
@@ -85,25 +87,23 @@ h1, h2, h3 {
 }
 
 h1 {
+  margin: 16px 0px 32px 0px;
   font-size: 6rem;
-  margin-top: 16px;
-  margin-bottom: 32px;
 }
 
 h2 {
-  font-size: 3.5rem;
+  font-size: 2.5rem;
 }
 
 h3 {
+  margin: 32px 0px 8px 0px;
   font-size: 2rem;
-  margin-top: 24px;
-  margin-bottom: 8px;
 }
 
 h4 {
   font-family: "Roboto";
   font-size: 1rem;
-  letter-spacing: .8px;
+  letter-spacing: .90px;
 }
 
 .abilities-container {
@@ -113,13 +113,20 @@ h4 {
 }
 
 .abilities-sub-container {
-  width: 55px;
-  height: 55px;
-  background-color: rgb(190, 190, 190, .70);
+  width: 60px;
+  height: 60px;
+  padding: 12px;
+  background-color: rgba(1, 1, 1, .40);
   border: 2px solid rgb(190, 190, 190);
   border-radius: 100%;
   transition: 0.3s;
   cursor: pointer;
+}
+
+.abilities-sub-container img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
 }
 
 .heroes-container {
@@ -129,13 +136,13 @@ h4 {
   flex-wrap: wrap;
   gap: 14px;
   margin: auto;
-  margin-top: 220px;
+  margin-top: 280px;
 }
 
 .heroes-sub-container {
   width: 55px;
   height: 55px;
-  background-color: rgba(1, 1, 1, .50);
+  background-color: rgba(1, 1, 1, .60);
   border: 2px solid rgb(190, 190, 190);
   border-radius: 6px;
   transition: 0.3s;
@@ -161,7 +168,7 @@ h4 {
 
 button {
   width: max-content;
-  padding: 12px 24px 8px 24px;
+  padding: 8px 24px 4px 24px;
   background-color: rgb(0, 220, 217);
   color: 2px solid rgb(255, 255, 255);
   font-size: 1.5rem;
